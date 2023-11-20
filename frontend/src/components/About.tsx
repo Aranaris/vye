4307063d879373c355fdf7d43837ba2c46677f9e
vinceye <vinceye.cm@gmail.com>
2023-11-20 13:04:15 -0800

    rewording about page

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -5,11 +5,13 @@ function About() {
 	return (
 		<section className='About'>
 			<header className='section-header'>Vince Ye</header>
-			<p>I am a product manager with extensive experience in the healthcare and chemical engineering industries developing enterprise SaaS solutions.
-			I graduated with a B.S. in Chemical Engineering from Carnegie Mellon University and have a passion for optimization and working on products that automate tedious workflows.</p>
+			<p>My experience includes working as a product manager at established organizations developing
+			enterprise SaaS solutions in the healthcare and chemical engineering industries. The platforms that I've worked on range from
+			entirely cloud-based web applications to on-prem solutions directly installed on client servers and desktops.</p>
 
-			<p>As a developer, I build applications and design APIs that provide easier ways to access data so that people can generate valuable
-			insights without having to spend the majority of their effort managing disparate and unstructured datasets.</p>
+			<p>As an engineering graduate from Carnegie Mellon, I have a passion for optimization and working on products that automate tedious workflows.
+			I develop applications and design APIs that provide easier ways to access data so that people can generate valuable
+			insights without having to spend the majority of their effort managing disparate data sources and unstructured datasets.</p>
 
 			<section className='past-experience'>
 				<section className='company-card'>
@@ -25,7 +27,7 @@ function About() {
 				<section className='company-card'>
 					<div className='logo'>img</div>
 					<div className='company-name'>athenahealth</div>
-					<div className='highlights'>Sr. Product Associate</div>
+					<div className='highlights'>Sr. Product Management/Analytics Associate</div>
 				</section>
 				<section className='company-card'>
 					<div className='logo'>img</div>
